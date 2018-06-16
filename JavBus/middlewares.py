f376c2979569b2e4713a6f4a96695f8d1d1441cb
Sowevo <i@Sowevo.com>
2018-06-16 16:37:30 +0800

    随机Agent的功能

--- JavBus/middlewares.py
+++ JavBus/middlewares.py
@@ -4,8 +4,12 @@
 #
 # See documentation in:
 # https://doc.scrapy.org/en/latest/topics/spider-middleware.html
+import random
 
 from scrapy import signals
+from scrapy.downloadermiddlewares.useragent import UserAgentMiddleware
+
+from JavBus.useragent import agents
 
 
 class JavbusSpiderMiddleware(object):
@@ -101,3 +105,10 @@ class JavbusDownloaderMiddleware(object):
 
     def spider_opened(self, spider):
         spider.logger.info('Spider opened: %s' % spider.name)
+
+
+class UserAgentmiddleware(UserAgentMiddleware):
+
+    def process_request(self, request, spider):
+        agent = random.choice(agents)
+        request.headers["User-Agent"] = agent
