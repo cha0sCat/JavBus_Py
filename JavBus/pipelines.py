15955d2ea3ebecc92da3a4b3e6f5cdfad1a370e9
cha0sCat <admin@noob.pw>
2019-12-26 01:37:29 +0800

    移除datastore调用

--- JavBus/pipelines.py
+++ JavBus/pipelines.py
@@ -10,7 +10,6 @@ import json
 import requests
 import pymongo
 import pymysql
-from google.cloud import datastore
 from scrapy.utils.project import get_project_settings
 settings = get_project_settings()
 
