62c95dac24065bbea8861f07a2b859b415ccebd6
cha0sCat <admin@noob.pw>
2019-12-26 21:44:28 +0800

    增加ACI启动容器指令

--- GCE_Tamplates.sh
+++ GCE_Tamplates.sh
@@ -20,3 +20,6 @@ docker run -itd --name javbus -e REDIS_URL=redis://user:RUtXSUag3PVZ@104.199.225
 docker version > /dev/null || curl -fsSL get.docker.com | bash
 service docker restart
 docker run -itd --name javbus -e REDIS_URL=redis://user:RUtXSUag3PVZ@104.199.225.82:6379 --log-opt max-size=50m --log-opt max-file=3 cha0scat/javbus_py
+
+## AZ CI
+az container create --image cha0scat/javbus_py --resource-group javbus --location eastasia --name javbus5 --os-type Linux --cpu 2 --memory 1 --ip-address public --ports 80 --environment-variables REDIS_URL=redis://user:RUtXSUag3PVZ@104.199.225.82:6379 --verbose
