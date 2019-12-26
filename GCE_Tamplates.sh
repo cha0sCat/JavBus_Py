96e956f316168613ca8583bfcbe68ca6bdf27547
cha0sCat <admin@noob.pw>
2019-12-26 10:52:00 +0800

    修改javbus数据的key

---
+++ GCE_Tamplates.sh
@@ -0,0 +1,22 @@
+apt-get update
+apt-get install git -y
+
+docker version > /dev/null || curl -fsSL get.docker.com | bash
+service docker restart
+
+cd /root
+git clone https://github.com/cha0sCat/JavBus_Py
+
+cd JavBus_Py
+git checkout datastore
+
+# docker pull python
+
+docker build -t javbus .
+docker run -itd --name javbus -e REDIS_URL=redis://user:RUtXSUag3PVZ@104.199.225.82:6379 javbus
+
+
+#### v2
+docker version > /dev/null || curl -fsSL get.docker.com | bash
+service docker restart
+docker run -itd --name javbus -e REDIS_URL=redis://user:RUtXSUag3PVZ@104.199.225.82:6379 --log-opt max-size=50m --log-opt max-file=3 cha0scat/javbus_py
